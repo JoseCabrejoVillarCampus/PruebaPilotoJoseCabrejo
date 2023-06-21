7b79c03db239c5bbf7c15099576c2be3676ddc07
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-21 07:58:59 -0500

    feat: :sparkles: CRUD TB areas
    Creacion de los metodos para la clase areas

---
+++ scripts/areas/areas.php
@@ -0,0 +1,73 @@
+<?php
+class areas extends connect
+{
+    private $queryPost = 'INSERT INTO areas(id) VALUES(:identificacion)';
+    private $queryGetAll = 'SELECT * FROM areas';
+    private $queryUpdate = 'UPDATE areas SET id = :identificacion  WHERE id = :id_areas';
+    private $queryDelete = 'DELETE FROM areas WHERE id = :identificacion';
+    private $message;
+    use getInstance;
+    function __construct(public $id=1)
+    {
+        parent::__construct();
+    }
+    public function postAreas()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("identificacion", $this->id);
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "inserted data"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function getAllAdminArea()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $res->bindValue("identificacion", $this->id);
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function putAdminArea()
+    {
+
+        try {
+            $res = $this->conx->prepare($this->queryUpdate);
+            $res->bindValue("identificacion", $this->id);
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200, "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "No matching record found"];
+            }
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function deleteAreas()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryDelete);
+            $res->bindValue("identificacion", $this->id);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Data delete"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+?>
