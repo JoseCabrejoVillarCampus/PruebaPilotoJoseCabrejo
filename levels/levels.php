194a8c0e4608021d6079fc92c1a62e2f56aec1d7
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-21 14:23:10 -0500

    feat: :sparkles: CRUD TB levels
    CReacion de los metodos para la tabla levels

---
+++ levels/levels.php
@@ -0,0 +1,81 @@
+<?php
+class levels extends connect
+{
+    private $queryPost = 'INSERT INTO levels(id,name_level,group_level) VALUES(:identificacion,:name,:group)';
+    private $queryGetAll = 'SELECT * FROM levels';
+    private $queryUpdate = 'UPDATE levels SET id = :identificacion, name_level = :name, group_level = :group  WHERE id = :identificacion';
+    private $queryDelete = 'DELETE FROM levels WHERE id = :identificacion';
+    private $message;
+    use getInstance;
+    function __construct(private $id=1, public $name_level=1, public $group_level=1)
+    {
+        parent::__construct();
+    }
+    public function postLevels()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("name",$this->name_level);
+            $res->bindValue("group", $this->group_level);
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "inserted data"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function getAllLevels()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("name",$this->name_level);
+            $res->bindValue("group", $this->group_level);
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function putLevels()
+    {
+
+        try {
+            $res = $this->conx->prepare($this->queryUpdate);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("name",$this->name_level);
+            $res->bindValue("group", $this->group_level);
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200, "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "No matching record found"];
+            }
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function deleteLevels()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryDelete);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("name",$this->name_level);
+            $res->bindValue("group", $this->group_level);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Data delete"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+?>
