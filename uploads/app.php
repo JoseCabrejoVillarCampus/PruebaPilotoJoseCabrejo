ae75e7d7c38951f10c3706e456de4944194e36c8
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-21 07:54:20 -0500

    feat: :rocket: Crud DB Campus
    Iniciamos la creacion del crud para la base de datos de capusland

---
+++ uploads/app.php
@@ -0,0 +1,45 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+trait getInstance
+{
+    public static $instance;
+    public static function getInstance()
+    {
+        $arg = func_get_args();
+        $arg = array_pop($arg);
+        return (!(self::$instance instanceof self) || !empty($arg)) ? self::$instance = new static(...(array) $arg) : self::$instance;
+    }
+    function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+}
+function autoload($class)
+{
+    // Directorios donde buscar archivos de clases
+    $directories = [
+        dirname(__DIR__) . '/scripts/academic_area/',
+        dirname(__DIR__) . '/scripts/admin_area/',
+        dirname(__DIR__) . '/scripts/areas/',
+        dirname(__DIR__) . '/scripts/seller/',
+        dirname(__DIR__) . '/scripts/db/'
+    ];
+    // Convertir el nombre de la clase en un nombre de archivo relativo
+    $classFile = str_replace('\\', '/', $class) . '.php';
+
+    // Recorrer los directorios y buscar el archivo de la clase
+    foreach ($directories as $directory) {
+        $file = $directory . $classFile;
+        // Verificar si el archivo existe y cargarlo
+        if (file_exists($file)) {
+            require $file;
+            break;
+        }
+    }
+}
+spl_autoload_register('autoload');
+
+client::getInstance(json_decode(file_get_contents("php://input"), true))->getAllClient();
+
+?>
