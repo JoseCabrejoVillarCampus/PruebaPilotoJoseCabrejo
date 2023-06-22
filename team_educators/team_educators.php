43c0d7750f45f8b9c4b5faeb5357b84187eb1d10
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-22 07:52:52 -0500

    feat: :sparkles: CRUD TB team_educators
    Creamos los metodos crud para la tabla team_educators

--- team_educators/team_educators.php
+++ team_educators/team_educators.php
@@ -1,23 +1,22 @@
 <?php
-class position extends connect
+class teameducators extends connect
 {
-    private $queryPost = 'INSERT INTO position(id,name_position,arl) VALUES(:identificacion,:posicion,:_arl)';
-    private $queryGetAll = 'SELECT * FROM position';
-    private $queryUpdate = 'UPDATE position SET id = :identificacion, name_position = :posicion, arl = :_arl  WHERE id = :identificacion';
-    private $queryDelete = 'DELETE FROM position WHERE id = :identificacion';
+    private $queryPost = 'INSERT INTO team_educators(id,name_rol) VALUES(:identificacion,:namerol)';
+    private $queryGetAll = 'SELECT * FROM team_educators';
+    private $queryUpdate = 'UPDATE team_educators SET id = :identificacion, name_rol = :namerol  WHERE id = :identificacion';
+    private $queryDelete = 'DELETE FROM team_educators WHERE id = :identificacion';
     private $message;
     use getInstance;
-    function __construct(private $id=1, public $name_position=1, public $arl=1)
+    function __construct(private $id=1, public $name_rol=1)
     {
         parent::__construct();
     }
-    public function postPosition()
+    public function postTeamEducators()
     {
         try {
             $res = $this->conx->prepare($this->queryPost);
             $res->bindValue("identificacion", $this->id);
-            $res->bindValue("posicion",$this->name_position);
-            $res->bindValue("_arl",$this->arl);
+            $res->bindValue("namerol",$this->name_rol);
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "inserted data"];
         } catch (\PDOException $e) {
@@ -26,14 +25,13 @@ class position extends connect
             print_r($this->message);
         }
     }
-    public function getAllPosition()
+    public function getAllTeamEducators()
     {
         try {
             $res = $this->conx->prepare($this->queryGetAll);
             $res->execute();
             $res->bindValue("identificacion", 3);
-            $res->bindValue("posicion", 1);
-            $res->bindValue("_arl", 1);
+            $res->bindValue("namerol", 1);
             $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
         } catch (\PDOException $e) {
             $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
@@ -41,14 +39,13 @@ class position extends connect
             print_r($this->message);
         }
     }
-    public function putPosition()
+    public function putTeamEducators()
     {
 
         try {
             $res = $this->conx->prepare($this->queryUpdate);
             $res->bindValue("identificacion", $this->id);
-            $res->bindValue("posicion",$this->name_position);
-            $res->bindValue("_arl",$this->arl);
+            $res->bindValue("namerol",$this->name_rol);
             $res->execute();
 
             if ($res->rowCount() > 0) {
@@ -62,13 +59,12 @@ class position extends connect
             print_r($this->message);
         }
     }
-    public function deletePosition()
+    public function deleteTeamEducators()
     {
         try {
             $res = $this->conx->prepare($this->queryDelete);
             $res->bindValue("identificacion", $this->id);
-            $res->bindValue("posicion",$this->name_position);
-            $res->bindValue("_arl",$this->arl);
+            $res->bindValue("namerol",$this->name_rol);
             $res->execute();
             $this->message = ["Code" => 200, "Message" => "Data delete"];
         } catch (\PDOException $e) {
