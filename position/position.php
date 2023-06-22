40bd242588f6ffc70b5d37a099668deea3d3eaa9
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-21 20:20:44 -0500

    feat: :sparkles: CRUD TB position
    Creamos los metodos dml para la tabla position

---
+++ position/position.php
@@ -0,0 +1,81 @@
+<?php
+class position extends connect
+{
+    private $queryPost = 'INSERT INTO position(id,name_position,arl) VALUES(:identificacion,:posicion,:_arl)';
+    private $queryGetAll = 'SELECT * FROM position';
+    private $queryUpdate = 'UPDATE position SET id = :identificacion, name_position = :posicion, arl = :_arl  WHERE id = :identificacion';
+    private $queryDelete = 'DELETE FROM position WHERE id = :identificacion';
+    private $message;
+    use getInstance;
+    function __construct(private $id=1, public $name_position=1, public $arl=1)
+    {
+        parent::__construct();
+    }
+    public function postPosition()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("posicion",$this->name_position);
+            $res->bindValue("_arl",$this->arl);
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "inserted data"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function getAllPosition()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $res->bindValue("identificacion", 3);
+            $res->bindValue("posicion", 1);
+            $res->bindValue("_arl", 1);
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function putPosition()
+    {
+
+        try {
+            $res = $this->conx->prepare($this->queryUpdate);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("posicion",$this->name_position);
+            $res->bindValue("_arl",$this->arl);
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200, "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "No matching record found"];
+            }
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function deletePosition()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryDelete);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("posicion",$this->name_position);
+            $res->bindValue("_arl",$this->arl);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Data delete"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+?>
