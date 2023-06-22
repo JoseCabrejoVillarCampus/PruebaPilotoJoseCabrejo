e070727b163ca24c13f363d9351583dadee08009
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-21 20:36:49 -0500

    feat: :sparkles: CRUD TB regions
    Creamos los metodos de crud para la tb regions

---
+++ regions/regions.php
@@ -0,0 +1,81 @@
+<?php
+class regions extends connect
+{
+    private $queryPost = 'INSERT INTO regions(id,name_region,id_country) VALUES(:identificacion,:namereg,:country)';
+    private $queryGetAll = 'SELECT * FROM regions';
+    private $queryUpdate = 'UPDATE regions SET id = :identificacion, name_region = :namereg, id_country = :country  WHERE id = :identificacion';
+    private $queryDelete = 'DELETE FROM regions WHERE id = :identificacion';
+    private $message;
+    use getInstance;
+    function __construct(private $id=1, public $name_region=1, private $id_country=1)
+    {
+        parent::__construct();
+    }
+    public function postRegions()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryPost);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("namereg",$this->name_region);
+            $res->bindValue("country",$this->id_country);
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "inserted data"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function getAllRegions()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $res->bindValue("identificacion", 3);
+            $res->bindValue("namereg", 1);
+            $res->bindValue("country", 1);
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function putRegions()
+    {
+
+        try {
+            $res = $this->conx->prepare($this->queryUpdate);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("namereg",$this->name_region);
+            $res->bindValue("country",$this->id_country);
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200, "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "No matching record found"];
+            }
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+    public function deleteRegions()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryDelete);
+            $res->bindValue("identificacion", $this->id);
+            $res->bindValue("namereg",$this->name_region);
+            $res->bindValue("country",$this->id_country);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => "Data delete"];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+?>
